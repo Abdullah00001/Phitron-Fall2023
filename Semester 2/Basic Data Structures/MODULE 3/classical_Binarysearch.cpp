baf034a82f1d38dac7ad494eccadebf4a0996374
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-12 19:19:31 +0600

    try to make binary search programme

---
+++ Semester 2/Basic Data Structures/MODULE 3/classical_Binarysearch.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int x;
+    cin >> x;
+    int l = 0, r = n - 1;
+    bool flag = false;
+    while (l <= r)
+    {
+        int mid_index = (l + r) / 2;
+    }
+
+    return 0;
+}
