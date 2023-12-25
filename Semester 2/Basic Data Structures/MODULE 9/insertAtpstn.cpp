7f2b979e0cb75c5b07f3cdd385078fbc28b55dcf
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-25 08:00:13 +0600

    finally understand

--- Semester 2/Basic Data Structures/MODULE 9/insertAtpstn.cpp
+++ Semester 2/Basic Data Structures/MODULE 9/insertAtpstn.cpp
@@ -1,7 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+void insrtAtPos(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->prev = tmp;
+    newNode->next->prev = newNode;
+}
+void prntfrwrd(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void prntbcwrd(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
 int main()
-{ 
-    
+{
+    Node *head = new Node(100);
+    Node *a = new Node(200);
+    Node *b = new Node(300);
+    Node *c = new Node(400);
+    Node *tail = c;
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+    int pos, val;
+    cin >> pos >> val;
+    insrtAtPos(head, pos, val);
+    prntfrwrd(head);
+    prntbcwrd(tail);
     return 0;
 }
