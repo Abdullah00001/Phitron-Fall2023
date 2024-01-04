9eacca19d671d2c6f4b617f85bc8fe068776318e
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2024-01-04 19:14:40 +0600

    btree right traverse print

---
+++ Semester 2/Basic Data Structures/MODULE 18/binaryTreeInput.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value=value;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+void inputB_Tree(Node *root)
+{
+    queue<Node*>q;
+    
+}
+int main()
+{ 
+    Node *root=NULL;
+
+    return 0;
+}
