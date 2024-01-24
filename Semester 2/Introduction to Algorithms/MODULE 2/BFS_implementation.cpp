f57eeaeb31444f4d06c3995ed0dba8b8997c7ec1
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2024-01-24 18:41:11 +0600

    finally understand

--- Semester 2/Introduction to Algorithms/MODULE 2/BFS_implementation.cpp
+++ Semester 2/Introduction to Algorithms/MODULE 2/BFS_implementation.cpp
@@ -1,16 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> bFtrav[1005];
+bool isVis[1005];
+void trav(int src)
+{
+    queue<int> p;
+    p.push(src);
+    isVis[src] = true;
+    while (!p.empty())
+    {
+        int prnt = p.front();
+        p.pop();
+        cout << prnt << endl;
+        for (int child : bFtrav[prnt])
+        {
+            if (!isVis[child])
+            {
+                p.push(child);
+                isVis[child] = true;
+            }
+        }
+    }
+}
 int main()
-{ 
-    int N,E;
-    cin>>N>>E;
-    vector<pair<int,int>>p[N];
+{
+    int N, E;
+    cin >> N >> E;
     while (E--)
     {
-        int A,B;
-        cin>>A>>B;
-        
+        int A, B;
+        cin >> A >> B;
+        bFtrav[A].push_back(B);
+        bFtrav[B].push_back(A);
     }
-    
+    int src;
+    cin >> src;
+    trav(src);
     return 0;
 }
