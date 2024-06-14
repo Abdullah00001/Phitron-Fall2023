a66c8ce0e4fa2c30679bb0adda9fb3577b8b2072
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-06-14 20:44:51 +0600

    adding login,signup,update profile view

--- Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-19.5/BBMS/account/views.py
+++ Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-19.5/BBMS/account/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from account.forms import SignUpForm, ProfileUpdateForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import FormView
+from django.views.generic import UpdateView
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+
+class SignUpPageView(FormView):
+    form_class = SignUpForm
+    template_name = "account/signup.html"
+    success_url = reverse_lazy("login")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        return context
+
+
+class LoginPageView(LoginView):
+    authentication_form = AuthenticationForm
+    template_name = "account/login.html"
+    success_url = reverse_lazy("profile")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        return context
+
+
+class ProfileUpdatePageView(UpdateView):
+    model = User
+    context_object_name = "form"
+    form_class = ProfileUpdateForm
+    template_name = "account/updateprofile.html"
+    success_url = reverse_lazy("profile")
+
+    def get_object(self):
+        return self.request.user
