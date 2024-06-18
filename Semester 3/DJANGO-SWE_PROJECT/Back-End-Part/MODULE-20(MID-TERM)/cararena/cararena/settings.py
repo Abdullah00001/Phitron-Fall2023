874699fb975ef604fc56a52e06ca23e8d11c348e
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-06-18 17:15:07 +0600

    add hero,brands and products section

--- Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-20(MID-TERM)/cararena/cararena/settings.py
+++ Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-20(MID-TERM)/cararena/cararena/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_URL = "/media/"
 LOGIN_URL = "/account/login/"
 
--- Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-20(MID-TERM)/cararena/home/views.py
+++ Semester 3/DJANGO-SWE_PROJECT/Back-End-Part/MODULE-20(MID-TERM)/cararena/home/views.py
@@ -1,8 +1,17 @@
+from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from carinfo.models import CarDetailModel
+from django.views.generic import ListView
+
 
 # Create your views here.
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = "home/home.html"
+    model=CarDetailModel
+    
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["cars"]=CarDetailModel.objects.order_by('?')[:8]
+        return context
